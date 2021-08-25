479cddf9ac2bfbd0b02ca7ffd67bb15ba166ee7a
Jang Haemin <io@jhaemin.com>
2021-08-25 23:44:09 +0900

    Just export everything from the generated directory

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,12 +1,1 @@
-import React from 'react'
-import { F7IconProps } from './types'
-import iconsMap from './generated/icons-map'
-
-const F7Icon: React.FC<F7IconProps> = (props) => {
-  const { name, ...iconProps } = props
-  const Icon = iconsMap[name]
-
-  return <Icon {...iconProps} />
-}
-
-export default F7Icon
+export * from './generated'
